62b4dd5e460e5120eeac1b9204fa57b60b3663e8
Poly Takahiro Horikawa <horikawa.takahiro@gmail.com>
2013-09-09 11:51:56 -0700

    Create Android Project

---
+++ android/jni/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+OPENCV_CAMERA_MODULES:=on
+OPENCV_INSTALL_MODULES:=on
+OPENCV_LIB_TYPE:=SHARED
+include $(OPENCV_ANDROID_HOME)/sdk/native/jni/OpenCV.mk
+
+LOCAL_SRC_FILES  := DetectionBasedTracker_jni.cpp
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_LDLIBS     += -llog -ldl
+
+LOCAL_MODULE     := n3
+
+include $(BUILD_SHARED_LIBRARY)
