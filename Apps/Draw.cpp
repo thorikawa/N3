942435e910b9e414c6e1446d4be35967896e85a0
Poly Takahiro Horikawa <horikawa.takahiro@gmail.com>
2013-09-14 13:09:07 -0700

    Accumurate draw path

--- Apps/Draw.cpp
+++ Apps/Draw.cpp
@@ -5,12 +5,16 @@ namespace Apps
 	Draw::Draw ():notFindCount(0),prevfind(false),color(CV_RGB(255,255,255)) {
 	}
 	
-	void Draw::trackMarker (Mat& destImg, Point& _r, Point& _b, Point& _g, Point& _y) {
+	void Draw::trackMarker (Mat& destImage, Point& _r, Point& _b, Point& _g, Point& _y) {
+        if (destImage.cols != image.cols || destImage.rows != image.rows) {
+            LOGD("resize image\n");
+            image = Mat(destImage.size(), destImage.type());
+        }
 		if (_r.x >= 0 && _b.x < 0) {
 			// find only red
 			//cvRectangle(dst, cvPoint(p.x-2, p.y-2), cvPoint(p.x+2, p.y+2), color, 3);
 			if (prevfind){
-				line(destImg, prevPoint, _r, color, THICKNESS);
+				line(image, prevPoint, _r, color, THICKNESS);
 			} else {
 				// if there is no prev point, do nothing
 			}
@@ -21,6 +25,8 @@ namespace Apps
 			if(++notFindCount >= ALLOW_ERROR_COUNT) {
 				prevfind = false;
 			}
-		}		
+		}
+        //image.copyTo(destImage);
+        addWeighted(destImage, 0.5, image, 0.5, 0.0, destImage);
 	}
 }
--- Apps/Draw.h
+++ Apps/Draw.h
@@ -2,6 +2,7 @@
 #define _DRAW_H_
 
 #include <opencv2/opencv.hpp>
+#include "../n3.h"
 
 using namespace cv;
 
@@ -10,12 +11,13 @@ namespace Apps
 	class Draw
 	{
 	private:
-    const static int ALLOW_ERROR_COUNT = 5;
-    const static int THICKNESS = 2;
-    int notFindCount;
-    bool prevfind;
-    Point prevPoint;
-    Scalar color;
+        const static int ALLOW_ERROR_COUNT = 5;
+        const static int THICKNESS = 2;
+        int notFindCount;
+        bool prevfind;
+        Point prevPoint;
+        Scalar color;
+        Mat image;
 	public:
 		Draw();
 		void trackMarker (Mat& destImg, Point& _r, Point& _b, Point& _g, Point& _y);
