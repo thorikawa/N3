7702e29cf93a77cbfe671ea97eb6776ae6686b94
Poly Takahiro Horikawa <horikawa.takahiro@gmail.com>
2013-12-15 23:03:07 -0800

    Add MarkerDetection code

---
+++ MarkerDetection/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH_ORIG := $(LOCAL_PATH)
+LOCAL_PATH := /Users/poly/workspace/N3/MarkerDetection
+
+include $(CLEAR_VARS)
+
+OPENCV_CAMERA_MODULES:=on
+OPENCV_INSTALL_MODULES:=on
+OPENCV_LIB_TYPE:=SHARED
+
+include /Users/poly/workspace/OpenCV-2.4.6-android-sdk/sdk/native/jni/OpenCV.mk
+
+LOCAL_SRC_FILES = CameraCalibration.cpp GeometryTypes.cpp Marker.cpp MarkerDetector.cpp TinyLA.cpp
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_LDLIBS     += -llog -ldl
+LOCAL_MODULE := n3_marker
+include $(BUILD_SHARED_LIBRARY)
+
+LOCAL_PATH := $(LOCAL_PATH_ORIG)
